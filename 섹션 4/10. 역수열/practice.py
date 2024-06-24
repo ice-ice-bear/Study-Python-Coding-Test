0932528b0f9e798553077912f97d502457d12901
ice-ice-bear <3balbi7@gmail.com>
2024-06-24 15:04:31 +0900

    solve section5 number1

--- 섹션 4/10. 역수열/practice.py
+++ 섹션 4/10. 역수열/practice.py
@@ -1,14 +1,36 @@
 import sys
-sys.stdin = open('input.txt', 'r')
+sys.stdin = open('in1.txt', 'r')
 
 n = int(input())
 a = list(map(int, input().split()))
 
 rev_seq_a= list(n*[0])
 
+def check_0(a, num):
+    cnt = 0
+    for i in range(num):
+        if a[i] == 0:
+            cnt += 1
+    return cnt
+
+
+def next_0(a, num):
+    cnt = 0
+    for i in range(len(a)):
+        if a[i] == 0:
+           cnt += 1
+           if cnt == num + 1:
+               return i 
+
 for idx, num in enumerate(a):
-    print(idx+1, num)
-    if rev_seq_a[num-1] == 0:
-        rev_seq_a[num-1] = idx+1
+    idx = idx + 1
+    if rev_seq_a[num] == 0:        
+        if check_0(rev_seq_a, num) == num:
+            rev_seq_a[num] = idx
+        else:
+            index = next_0(rev_seq_a, num)
+            rev_seq_a[index] = idx
+    
+
 
 print(rev_seq_a)
